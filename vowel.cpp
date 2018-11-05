6b58a00162fb1b98a7465a4bb3237981783998b5
Nandhinihide <41441366+Nandhinihide@users.noreply.github.com>
2018-11-05 10:07:29 +0530

    Update vowel.cpp

--- vowel.cpp
+++ vowel.cpp
@@ -5,8 +5,10 @@ main()
   char mychar;
   cout<<"Enter the character"<<endl;
   cin>>mychar;
-  if(mychar=='a'||'e'||'i'||'o'||'u')
+  if(mychar=='a'||mychar=='e'||mychar=='i'||mychar=='o'||mychar=='u'||mychar=='A'||mychar=='E'||mychar=='I'||mychar=='O'||mychar=='U')
     cout<<"vowel";
+  if else((mychar>='a' && mychar<='z')||(mychar>='A' && mychar<='Z'))
+    cout<<"Consonant";
   else
     cout<<"Invalid";
 }
